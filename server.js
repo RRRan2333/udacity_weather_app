a0e91d6875a56493eb4351c5197318ffd9afd21b
Geran Wen <skran711g@gmail.com>
2020-05-20 22:45:01 -0500

    populated basics of .js files

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
 // Require Express to run server and routes
 const express = require('express');
 // Start up an instance of app
@@ -15,5 +14,24 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-// something
 // Setup Server
+const port = 3000;
+const server = app.listen(port,() => {
+    console.log(`running on localhost: ${port}`);
+});
+
+// GET route
+app.get('/receive', (req, res) => {
+    res.send(projectData);
+});
+
+//POST route
+app.post('/add', (req, res) => {
+    res.send('POST received');
+})
+
+// POST and update
+const data = [];
+app.post('/update', (req, res) => {
+    data.push(req.body);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,52 @@
-/* Global Variables */
+import { response } from "express";
 
+/* Global Variables */
+let baseURL = 'api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid=';
+let apiKey = '70deec56cd857531f08dd8699518fcb8';
+const newZipcode = ''; //doc get Element by ID or sth. 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+//Async POST
+const postData = async (url = '', data = {}) =>{
+
+    const res = await fetch(url, {
+        method: 'POST',
+        crodentials: 'same-origin',
+        headers: {
+            'Content-Type': 'applicaiton/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try{
+        const newData = await res.json();
+        return newData;
+    }catch(error){
+        console.log('error', error);
+    }
+};
+
+//Async GET
+const retrieveData = async (url='') => {
+    const req = await fetch(url); 
+    try {
+        //transform into JSON
+        const allData = await req.json();
+    }catch(error){
+        console.log('error', error);
+    }
+}
+
+//Chain async functions to do stuff
+// Features
+// 1: gather everything from HTML after "generate" (zipcode & feeling).
+// 2: GET weather via API with zipcode. 
+// 3: PUSH full post (object) to server.js
+// 4: GET full post (object) and update to HTML/UI.
+updateUI = () => {
+    console.log('updateUI function triggered');
+    
+}
